c7881dd6ec0b6dab44aabd956d27bf53d88ab450
Kamil Malinowski <malinowskikam96@gmail.com>
2024-12-02 20:10:55 +0100

    Day 1 part 1

--- 2024/day01/main.cpp
+++ 2024/day01/main.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
+#include <util.hpp>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+
+void part1();
+void part2();
 
 int main() {
-    std::cout << "Hello, World" << std::endl;
+    part1();
+}
 
-    exit(0);
+void part1() {
+    int sum = 0;
+
+    std::vector<int> left;
+    std::vector<int> right;
+
+    int l, r;
+
+    std::ifstream input = open_input("input/input01.txt");
+    while (input >> l >> r) {
+        left.push_back(l);
+        right.push_back(r);
+    }
+    
+    std::sort(left.begin(), left.end());
+    std::sort(right.begin(), right.end());
+
+    for (int i = 0; i < left.size(); i++) {
+        sum += abs(left[i] - right[i]);
+    }
+
+    std::cout << sum << std::endl;
 }
