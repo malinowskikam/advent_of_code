bfd08716fba0922e0a6bbba2e3919f72d427eab1
Kamil Malinowski <malinowskikam96@gmail.com>
2024-12-02 20:22:42 +0100

    Day 1 part 2

--- 2024/day01/main.cpp
+++ 2024/day01/main.cpp
@@ -1,14 +1,16 @@
+#include <algorithm>
 #include <iostream>
+#include <math.h>
+#include <unordered_map>
 #include <util.hpp>
 #include <vector>
-#include <algorithm>
-#include <math.h>
 
 void part1();
 void part2();
 
 int main() {
     part1();
+    part2();
 }
 
 void part1() {
@@ -24,12 +26,36 @@ void part1() {
         left.push_back(l);
         right.push_back(r);
     }
-    
+
     std::sort(left.begin(), left.end());
     std::sort(right.begin(), right.end());
 
     for (int i = 0; i < left.size(); i++) {
         sum += abs(left[i] - right[i]);
+    }
+
+    std::cout << sum << std::endl;
+}
+
+void part2() {
+    int sum = 0;
+
+    std::vector<int> left;
+    std::unordered_map<int, int> right;
+
+    int l, r;
+
+    std::ifstream input = open_input("input/input01.txt");
+    while (input >> l >> r) {
+        left.push_back(l);
+        right[r]++;
+    }
+
+    for (int i = 0; i < left.size(); i++) {
+        int n = left[i];
+        int mul = right[n];
+
+        sum += n * mul;
     }
 
     std::cout << sum << std::endl;
