8812e022469862949cb25c0372d20886f9d3b294
Kamil Malinowski <malinowskikam96@gmail.com>
2024-12-10 11:56:25 +0100

    Day 7 part 2

--- 2024/day07/main.cpp
+++ 2024/day07/main.cpp
@@ -3,14 +3,13 @@
 #include <string>
 #include <util.hpp>
 #include <vector>
+#include <cmath>
 
 void part1();
 void part2();
 
 bool check_line(const std::vector<long> &numbers, long value);
-bool check_line_req(const std::vector<long> &numbers, long value,
-                    long current_value,
-                    std::vector<long>::size_type current_index);
+bool check_line2(const std::vector<long> &numbers, long value);
 
 int main() {
     part1();
@@ -32,7 +31,6 @@ void part1() {
 
         if (check_line(numbers, value)) {
             sum += value;
-            std::cout << sum << std::endl;
         }
     }
 
@@ -40,9 +38,24 @@ void part1() {
 }
 
 void part2() {
-    int count = 0;
+    long sum = 0;
 
-    std::cout << count << std::endl;
+    std::string line;
+    std::ifstream is = open_input("input/input07.txt");
+    while (std::getline(is, line)) {
+        std::vector<std::string> split_val_and_nums = split(line, ": ");
+        long value = std::stol(split_val_and_nums[0]);
+        std::vector<long> numbers;
+        for (std::string s : split(split_val_and_nums[1], " ")) {
+            numbers.push_back(std::stol(s));
+        }
+
+        if (check_line2(numbers, value)) {
+            sum += value;
+        }
+    }
+
+    std::cout << sum << std::endl;
 }
 
 bool check_line(const std::vector<long> &numbers, long value) {
@@ -60,6 +73,37 @@ bool check_line(const std::vector<long> &numbers, long value) {
             }
             val >>= 1;
         }
+        if (result == value) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool check_line2(const std::vector<long> &numbers, long value) {
+    assert(numbers.size() > 0 && numbers.size() <= 32);
+
+    int n = pow(3, numbers.size() - 1);
+    for (int i = 0; i < n; i++) {
+        long result = numbers[0];
+        int val = i;
+        
+        for (auto it = numbers.begin() + 1; it != numbers.end(); ++it) {
+            switch (val % 3) {
+            case 0:
+                result += *it;
+                break;
+            case 1:
+                result *= *it;
+                break;
+            case 2:
+                result = std::stol(std::to_string(result) + std::to_string(*it));
+                break;
+            }
+
+            val /= 3;
+        }
+
         if (result == value) {
             return true;
         }
